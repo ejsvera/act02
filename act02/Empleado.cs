d602a653762d0499931be71766ee0304ec280b6d
enrique <enrique@leica>
2019-03-27 17:18:37 -0300

    01

---
+++ act02/Empleado.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace act02
+{
+    class Empleado : Persona
+    {
+        internal Jefe Jefe
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        internal Departamento Departamento
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+    }
+}
